12037cbca4435eb6004ad1ba5aa12cbe4b8eb689
Adam Garrett <admgarrett@gmail.com>
2012-12-20 21:05:21 -0500

    initial commit with 1.5ghz OC

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Dec 16 13:34:31 EST 2012"
+#define LINUX_COMPILE_BY "adam"
+#define LINUX_COMPILE_HOST "adam-dev"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
