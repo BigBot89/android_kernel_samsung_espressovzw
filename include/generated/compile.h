c147515022599cecc6660839d06cd0a9bf2d017e
Adam Garrett <admgarrett@gmail.com>
2012-12-21 18:03:22 -0500

    1.62ghz OC

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Sun Dec 16 13:34:31 EST 2012"
+#define UTS_VERSION "#9 SMP PREEMPT Fri Dec 21 17:36:16 EST 2012"
 #define LINUX_COMPILE_BY "adam"
 #define LINUX_COMPILE_HOST "adam-dev"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.8-DoubleShot-v1.1"
+#define UTS_RELEASE "3.0.8-DoubleShot-v1.4-g12037cb-dirty"
